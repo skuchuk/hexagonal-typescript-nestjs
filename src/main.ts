213282ff8c9af0f9c4a075fa495d1a3bbfc9bd92
shahark <shahar.kuchuk@cellebrite.com>
2021-02-08 14:55:39 +0200

    support config and env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require('config');
+
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(config.get('port'));
 }
 bootstrap();
